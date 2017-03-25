ed699ef4a87accee435d909195d4857c758b97df
gaurav1singh <gauravosingh1997@gmail.com>
2017-03-25 18:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img=document.getElementById('madi');
 var marginLeft= 0 ;   
 function moveRight () {
     marginLeft = marginLeft + 10;
-    img.style.marginLEFT=marginLeft+'px;'
+    img.style.marginLEFT=marginLeft+'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,10);
