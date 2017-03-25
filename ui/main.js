33c220f7c3896edf3e5cdb8ef0ac5e591a2b865f
gaurav1singh <gauravosingh1997@gmail.com>
2017-03-25 18:45:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
 var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft+'px;'
+var marginLeft= 0 ;   
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLEFT=marginLeft+'px;'
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,10);
